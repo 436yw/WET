d14e44e29fa22cbb06abe0978c5620516392aaa4
liqi <>
2021-07-21 16:43:24 +0800

    update

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,11 @@
 for i in range(1,6):
     print(' '*(6-i),end='')
     print('*'*(i*2-1))
+    print("更新的的代码")
+    print("更新的的代码")
+    print("更新的的代码")
+    print("更新的的代码")
+    print("更新的的代码")
+    print("更新的的代码")
+    print("更新的的代码")
+    
