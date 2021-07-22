2102b469500f7a25b3cae75c855afc15c1e70c3e
liqi <>
2021-07-22 13:56:30 +0800

    update

--- scripts/test.py
+++ scripts/test.py
@@ -1,11 +1,4 @@
 for i in range(1,6):
     print(' '*(6-i),end='')
     print('*'*(i*2-1))
-    print("更新的的代码")
-    print("更新的的代码")
-    print("更新的的代码")
-    print("更新的的代码")
-    print("更新的的代码")
-    print("更新的的代码")
-    print("更新的的代码")
-    
+    print("2021-7-22 13:56:03上传的代码")
